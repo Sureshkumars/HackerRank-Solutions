6697f4ac15d6536bbba167fc2756ecf62b6304c1
Suresh Kumar Sivasankaran <sureshkumarcool@live.com>
2015-10-07 19:36:53 +0530

    Create chocolate feast.c

---
+++ chocolate feast.c
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, c, m;
+		cin >> n >> c >> m;
+		int ans = 0, cur = n / c, rem = 0;
+		while (cur != 0)
+		{
+			ans += cur;
+			int tmp = (cur + rem) / m;
+			rem = cur + rem - tmp * m;
+			cur = tmp;
+		}
+		cout << ans << endl;
+	}
+}
